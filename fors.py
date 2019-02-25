12add3a61bbbdddcdbb2c5f0aef846c6f6a2f626
aaronBurns59 <aaronburns59@gmail.com>
2019-02-25 19:19:01 +0000

    basic code for while and for loops

---
+++ fors.py
@@ -0,0 +1,7 @@
+l = [0,1,2,3,4,5,6,7,8,9]
+
+for i in l:
+    sq= i*i
+    print("the Square of ", i, " is ", sq)
+
+print("out of loop")
---
+++ whiles.py
@@ -0,0 +1,11 @@
+i = int(input("Enter an positive integer: ")) 
+
+total = 0
+
+while i <= 0: 
+    i =int(input("Enter an positive integer: ")) 
+
+while i > 0:
+    total = total + 1
+    i = i -1
+    print(total)
